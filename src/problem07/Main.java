be60636705f58ff539afcf54bf739213a457bb32
Samiulopps <coding.samiul@gmail.com>
2023-07-28 19:42:38 +0600

    first commit after solving first three problem Second commit(Total Six problem has been solved) All problems of assignment-01 is solved;

---
+++ src/problem07/Main.java
@@ -0,0 +1,14 @@
+package problem07;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String str = input.nextLine();
+        System.out.println("Frequently used word: " + Solver.MaxUsedWord(str));
+        System.out.println("Word that has maximum length: " + Solver.findMaxLengthWord(str));
+    }
+}
