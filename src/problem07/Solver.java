2b6bfeb5cddf641723d30a843499a56539ea2142
Samiulopps <coding.samiul@gmail.com>
2023-08-05 01:40:38 +0600

    first commit after solving first three problem Second commit(Total Six problem has been solved)

--- src/problem07/Solver.java
+++ src/problem07/Solver.java
@@ -1,50 +1,39 @@
 package problem07;
 
-import java.util.HashMap;
 public class Solver {
-    protected static String MaxUsedWord(String str){
-        //splitting the line into word specified by whitespace
+
+    public static String MaxUsedWord(String str){
+
         String[] arr = str.split(" ");
 
-        //store all words into the map to identify how many times the specific word is used
-        int n = arr.length;
-        HashMap<String, Integer> wordMap = new HashMap<>();
-        for (int i = 0; i < n; i++){
-            if(!arr[i].isBlank()){      //ignoring element that contains whitespace only
-                if(wordMap.containsKey(arr[i])){    /* updating the value against a word
-                                                         that is already mapped into the map
-                                                    */
-                    int oldValue = wordMap.get(arr[i]);
-                    wordMap.replace(arr[i], oldValue+1);
-                }
-                else{                               //putting values against the key that are not exit in the map
-                    wordMap.put(arr[i], 1);
-                }
-            }
-        }
-        //Finding the word that are used most frequently
-        String temp = "";
-        int maxValues = 0;
-        for (String key:wordMap.keySet()) {
-            if(maxValues < wordMap.get(key)){
-                maxValues = wordMap.get(key);
-                temp = key;
-            }
-        }
-        return temp;
-    }
-
-    protected static String findMaxLengthWord(String str){
-        String[] arr = str.split(" ");
-        int n = arr.length, maxLength = 0;
+        int count = 0, max_count = 0;
         String word = "";
-        for(int i = 0; i < n; i++){
-            if(!arr[i].isBlank() && arr[i].length() > maxLength){
-                maxLength = arr[i].length();
+        for(int i = 0; i < arr.length; i++){
+            count = 0;
+            for(int j = 0; j < arr.length; j++){
+                if(arr[j].equals(arr[i])){
+                    count++;
+                }
+            }
+            if(count > max_count){
+                max_count = count;
                 word = arr[i];
             }
         }
 
         return word;
     }
+
+    public static String wordWithMaxLenght(String str){
+
+        String[] arr = str.split(" ");
+        String temp = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(temp.length() < arr[i].length()){
+                temp = arr[i];
+            }
+        }
+        return temp;
+    }
+
 }
