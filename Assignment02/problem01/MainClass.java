cdbcdf32b1541095fab3a6107ef694cdb036c282
Samiulopps <coding.samiul@gmail.com>
2023-08-08 15:04:23 +0600

    Assignment02/problemo2 solved

--- Assignment02/problem01/MainClass.java
+++ Assignment02/problem01/MainClass.java
@@ -6,16 +6,18 @@ import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
         StudentRecordManager[] myStudentList = new StudentRecordManager[100];
+        boolean not_initialize = true;
         int index = 0;
         Scanner input = new Scanner(System.in);
         while (true){
             System.out.printf("Add Student Info(1)\tDisplay Student Info(2)\tExit(other number)\n");
             int n = input.nextInt();
             if(n == 1){
+                not_initialize = false;
                 System.out.print("Enter Student ID: ");
                 int id = input.nextInt();
                 System.out.print("Enter Student Name: ");
-                String name = input.next().toUpperCase();
+                String name = new Scanner(System.in).nextLine().toUpperCase();
                 System.out.print("Enter the number of subjects: ");
                 int numberOfSubjects = input.nextInt();
                 ArrayList<Integer> markList = new ArrayList<>(numberOfSubjects);
@@ -26,22 +28,28 @@ public class MainClass {
                 myStudentList[index++] = new StudentRecordManager(id, name, markList);
             }
             else if (n == 2) {
-                System.out.println("Enter valid ID/Name to see the details: ");
-                String key = input.next();
-                boolean notFound = true;
-                for (int i = 0; i < index; i++){
-                    if( myStudentList[i].getName().equals(key.toUpperCase())){
-                        notFound = false;
-                        myStudentList[i].displayStudentDetails();
-                    }
-                    else if(key.charAt(0) >= '0' && key.charAt(0) <= '9' && (myStudentList[i].getStudentId() == Integer.valueOf(key))){
-                        notFound = false;
-                        myStudentList[i].displayStudentDetails();
-                        break;
-                    }
+                if(not_initialize){
+                    System.out.println("List is Empty");
                 }
-                if(notFound){
-                    System.out.println("please insert correct name or id");
+                else{
+
+                    System.out.println("Enter valid ID/Name to see the details: ");
+                    String key = input.next();
+                    boolean notFound = true;
+                    for (int i = 0; i < index; i++){
+                        if( myStudentList[i].getName().contains(key.toUpperCase())){
+                            notFound = false;
+                            myStudentList[i].displayStudentDetails();
+                        }
+                        else if(key.charAt(0) >= '0' && key.charAt(0) <= '9' && (myStudentList[i].getStudentId() == Integer.valueOf(key))){
+                            notFound = false;
+                            myStudentList[i].displayStudentDetails();
+                            break;
+                        }
+                    }
+                    if(notFound){
+                        System.out.println("please insert correct name or id");
+                    }
                 }
             }
             else{
--- Assignment02/problem02/MainClass.java
+++
@@ -1,7 +0,0 @@
-package problem02;
-
-public class MainClass {
-    public static void main(String[] args) {
-        System.out.println("This will be solved later");
-    }
-}
