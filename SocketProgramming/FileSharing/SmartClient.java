c83a7d8db03088d95559c022864560458b92754a
Samiulopps <coding.samiul@gmail.com>
2023-10-21 03:10:53 +0600

    Socket Programming (file transfer from server to client) example added. Any type of files can be shared

---
+++ SocketProgramming/FileSharing/SmartClient.java
@@ -0,0 +1,33 @@
+package FileSharing;
+
+import java.io.*;
+import java.net.*;
+
+public class SmartClient {
+    public static void main(String[] args) throws Exception{
+        try{
+            Socket clientSoc = new Socket("localhost", 105);
+            DataInputStream inputStream = new DataInputStream(clientSoc.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(clientSoc.getOutputStream());
+
+            String fileName = "SocketProgramming/FileSharing/output" + inputStream.readUTF();
+            File file = new File(fileName);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            if(file.exists()){
+                file.delete();
+            }
+            file.createNewFile();
+            int c;
+            while((c = inputStream.read()) != -1){
+                System.out.println((char)c);
+                fileOutputStream.write((char)c);
+            }
+            fileOutputStream.close();
+            inputStream.close();
+            clientSoc.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+}
---
+++ SocketProgramming/FileSharing/SmartServer.java
@@ -0,0 +1,40 @@
+package FileSharing;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SmartServer {
+    public static void main(String[] args) throws Exception{
+        try{
+            ServerSocket serverSocket = new ServerSocket(105);
+            System.out.println("Server is running..");
+            Socket socket = serverSocket.accept();
+            System.out.println("Connected successfully to the client");
+
+            File file = new File("SocketProgramming/FileSharing/myVideo.mp4");
+            String fileName = file.getName();
+            String extension = fileName.substring(fileName.lastIndexOf("."));
+            System.out.println(extension);
+
+            FileInputStream fileInputStream = new FileInputStream(file);
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            outputStream.writeUTF(extension);
+
+            int c;
+            while((c=fileInputStream.read()) != -1){
+                outputStream.write(c);
+            }
+            System.out.println("file sent successfully");
+            outputStream.close();
+            fileInputStream.close();
+            socket.close();
+            serverSocket.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
